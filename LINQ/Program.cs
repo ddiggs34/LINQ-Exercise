06e60b3df5f2414f58025cb85ee08804ed950cbf
David Diggs <ddiggs34@gmail.com>
2023-10-17 11:46:08 -0400

    done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace LINQ
+﻿using System.Collections.Specialized;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { /*Create a list of video game names... - DONE
+            Order the list of games by length of the game name.
+            Use the lambda expression in this exercise as well.
+            Use Method Syntax for this exercise*/
+            
+            var videogames = new string[] { "Call Of Duty", "Super Mario", "Sonic", "NBA 2K", "Final Fantasy", "Suikoden" }; // create a list
+            var gameNamesOrder = videogames.OrderBy(videogames => videogames.Length).ToList(); //orders the list by LENGTH
+            
+            foreach ( var gameName in gameNamesOrder ) 
+            {
+                Console.WriteLine(gameName);
+            }
         }
     }
 }
